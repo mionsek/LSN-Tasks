79282e99fcb6d4140b051f7c98fd2bc2c12d2c2f
Dawid <mionskowski.dawid@gmail.com>
2024-01-18 14:56:12 +0100

    Optimized 3rd task according to wiki - disjoint set data structure

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,17 @@ public class Main {
         FileReader reader = new FileReader();
         FileWriter writer = new FileWriter();
 
-        Task1 task1 = new Task1(reader.readOneLinerFileToList(Constants.INPUT_PATH_1));
-        System.out.println("\nTask1:");
-        task1.printResults();
-        writer.saveToFile(Constants.OUTPUT_PATH_1, task1.toString());
+//        Task1 task1 = new Task1(reader.readOneLinerFileToList(Constants.INPUT_PATH_1));
+//        System.out.println("\nTask1:");
+//        task1.printResults();
+//        writer.saveToFile(Constants.OUTPUT_PATH_1, task1.toString());
 
-        Task2 task2 = new Task2(reader.readOneLinerFileToList(Constants.INPUT_PATH_2));
-        System.out.println("\nTask2:");
-        task2.printResults();
-        writer.saveToFile(Constants.OUTPUT_PATH_2, task2.toString());
+//        Task2 task2 = new Task2(reader.readOneLinerFileToList(Constants.INPUT_PATH_2));
+//        System.out.println("\nTask2:");
+//        task2.printResults();
+//        writer.saveToFile(Constants.OUTPUT_PATH_2, task2.toString());
 
-        Task3 task3 = new Task3(reader.readGraphFileToSet(Constants.TEST_PATH));
+        Task3 task3 = new Task3(reader.readGraphFileToSet(Constants.INPUT_PATH_3));
         System.out.println("\nTask3:");
         task3.printResults();
         writer.saveToFile(Constants.OUTPUT_PATH_3, task3.toString());
--- src/task3/Task3.java
+++ src/task3/Task3.java
@@ -2,82 +2,116 @@ package task3;
 
 import constants.Constants;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Task3 {
     Set<Pair> connectionsSet;
     HashSet<TreeSet<Integer>> createdConnections;
 
+    private Map<Integer, Integer> parent;
+    private final int numberOfGraphs;
+
     public Task3(Set<Pair> connectionsSet) {
         this.connectionsSet = connectionsSet;
         createdConnections = new HashSet<>();
 
-        createGraphs();
+//        createGraphs();
+
+        numberOfGraphs = findNumberOfSeparateGraphs(connectionsSet);
     }
 
-    public void createGraphs() {
-        for (Pair pair : connectionsSet) {
-            boolean foundLeft = false, foundRight = false;
-            for (TreeSet<Integer> singleSet : createdConnections) {
-                if (singleSet.contains(pair.getLeft())) foundLeft = true;
-                if (singleSet.contains(pair.getRight())) foundRight = true;
-            }
-            if (!foundLeft && !foundRight)
-                addNewGraph(pair);
-            else if (foundLeft && !foundRight)
-                addToExistingGraph(pair, Constants.LEFT);
-            else if (!foundLeft && foundRight)
-                addToExistingGraph(pair, Constants.RIGHT);
-            else if (foundLeft && foundRight) // i know its redundant, but looks prettier
-                mergeGraphs(pair);
+    public int findNumberOfSeparateGraphs(Set<Pair> verticesSet) {
+        parent = new HashMap<>();
+        for (var twoVertices : verticesSet) {
+            int left = find(twoVertices.getLeft());
+            int right = find(twoVertices.getRight());
+            parent.put(left, right);
         }
+
+        return findAllSeparateGraphs();
     }
 
-    private void addNewGraph(Pair pair) {
-        createdConnections.add(new TreeSet<>(pair.asList()));
+    int find(int point) {
+        int p = parent.getOrDefault(point, point);
+        if (p != point)
+            p = find(p);
+        parent.put(point, p);
+        return p;
     }
 
-    private void addToExistingGraph(Pair pair, String side) {
-        for (TreeSet<Integer> singleSet : createdConnections) {
-            if (side.equals(Constants.LEFT) && singleSet.contains(pair.getLeft()))
-                singleSet.add(pair.getRight());
-            if (side.equals(Constants.RIGHT) && singleSet.contains(pair.getRight()))
-                singleSet.add(pair.getLeft());
-        }
+    private int findAllSeparateGraphs() {
+//        return Math.toIntExact(parent.values().stream().distinct().count());
+        int separateGraphs = 0;
+        for (var k : parent.keySet())
+            if (find(k) == k)
+                separateGraphs += 1;
+        return separateGraphs;
     }
 
-    private void mergeGraphs(Pair pair) {
-        // Actually I could skip it there because earlier there was a condition - 'contains'
-        Optional<TreeSet<Integer>> leftSetOptional = getOptional(pair.getLeft());
-        Optional<TreeSet<Integer>> rightSetOptional = getOptional(pair.getRight());
-
-        if (leftSetOptional.isPresent() && rightSetOptional.isPresent()){
-            TreeSet<Integer> leftSet = leftSetOptional.get();
-            TreeSet<Integer> rightSet = rightSetOptional.get();
-
-            rightSet.addAll(leftSet);
-
-            createdConnections.removeIf(singleSet -> singleSet.contains(pair.getLeft()));
-            createdConnections.removeIf(singleSet -> singleSet.contains(pair.getRight()));
-            createdConnections.add(rightSet);
-
-        }
+    public int getNumberOfGraphs() {
+        return numberOfGraphs;
     }
 
-    private Optional<TreeSet<Integer>> getOptional(Integer pair) {
-        return createdConnections
-                .stream()
-                .filter(singleSet -> singleSet.contains(pair))
-                .findFirst();
-    }
+
+//    public void createGraphs() {
+//        for (Pair pair : connectionsSet) {
+//            boolean foundLeft = false, foundRight = false;
+//            for (TreeSet<Integer> singleSet : createdConnections) {
+//                if (singleSet.contains(pair.getLeft())) foundLeft = true;
+//                if (singleSet.contains(pair.getRight())) foundRight = true;
+//            }
+//            if (!foundLeft && !foundRight)
+//                addNewGraph(pair);
+//            else if (foundLeft && !foundRight)
+//                addToExistingGraph(pair, Constants.LEFT);
+//            else if (!foundLeft && foundRight)
+//                addToExistingGraph(pair, Constants.RIGHT);
+//            else if (foundLeft && foundRight) // I know its redundant, but looks prettier
+//                mergeGraphs(pair);
+//        }
+//    }
+
+//    private void addNewGraph(Pair pair) {
+//        createdConnections.add(new TreeSet<>(pair.asList()));
+//    }
+
+//    private void addToExistingGraph(Pair pair, String side) {
+//        for (TreeSet<Integer> singleSet : createdConnections) {
+//            if (side.equals(Constants.LEFT) && singleSet.contains(pair.getLeft()))
+//                singleSet.add(pair.getRight());
+//            if (side.equals(Constants.RIGHT) && singleSet.contains(pair.getRight()))
+//                singleSet.add(pair.getLeft());
+//        }
+//    }
+//
+//    private void mergeGraphs(Pair pair) {
+//        // Actually I could skip it there because earlier there was a condition - 'contains'
+//        Optional<TreeSet<Integer>> leftSetOptional = getOptional(pair.getLeft());
+//        Optional<TreeSet<Integer>> rightSetOptional = getOptional(pair.getRight());
+//
+//        if (leftSetOptional.isPresent() && rightSetOptional.isPresent()) {
+//            TreeSet<Integer> leftSet = leftSetOptional.get();
+//            TreeSet<Integer> rightSet = rightSetOptional.get();
+//
+//            rightSet.addAll(leftSet);
+//
+//            createdConnections.removeIf(singleSet -> singleSet.contains(pair.getLeft()));
+//            createdConnections.removeIf(singleSet -> singleSet.contains(pair.getRight()));
+//            createdConnections.add(rightSet);
+//
+//        }
+//    }
+
+//    private Optional<TreeSet<Integer>> getOptional(Integer pair) {
+//        return createdConnections
+//                .stream()
+//                .filter(singleSet -> singleSet.contains(pair))
+//                .findFirst();
+//    }
 
 
     public void printResults() {
-        System.out.println(this);
+        System.out.println(getNumberOfGraphs());
     }
 
     @Override
